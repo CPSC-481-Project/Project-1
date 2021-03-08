e3723ffa6c7ba6e62c51873bd1b0463dd19598b9
Sagar Joshi <55200206+sagar0698@users.noreply.github.com>
2021-03-07 22:21:10 -0800

    Fuck BFS

--- wolfgoatcabbage_fancyprint.py
+++ wolfgoatcabbage_fancyprint.py
@@ -1,16 +1,12 @@
 from search import *
-# YOUR CODE GOES HERE
 
 # Constructor setting initial and goal states
 
-possible_actions_fancy = []
+
 class WolfGoatCabbage(Problem):
     # (F, W, G, C)
-
-    
-    def __init__(self, initial=(1, 0, 0, 0, True), goal=(1, 1, 1, 1, False)):
-        self.initial = initial
-        self.goal = goal
+    def __init__(self, initial_state, goal=(1, 1, 1, 1, False)):
+        super().__init__(initial_state, goal)
         # False --> boat is moving back to the right
         # True --> boat is moving to the left
 
@@ -20,55 +16,36 @@ class WolfGoatCabbage(Problem):
     def result(self, state, action):
        # returns the new state reached from the given state and the given action
         new_state = list(state)
-    #    Possible actions on the boat
+        # Possible actions on the boat
         # (F, W, G, C, Position)
-        # if action == "WCFG":
         # Keep track of states
-        if action[0] == "[C,F]":
-            # Keep track of states
-            if new_state[4] == True:
-                # Boat currently on left island and will move Cabbage to the right island
-                new_state[3] = 1
-                new_state[4] = not new_state[4]
-            else:
-                # Boat currently on the right island and will move Cabbage to the left island
-                new_state[3] = 0
-        elif action[0] == "[W,F]":
-            # Keep track of states
-            if new_state[4] == True:
-                # Boat currently on the left island and will move Wolf to the right island
-                new_state[1] = 1
-                new_state[4] = not new_state[4]
-            else:
-                # Boat currently on the right island and will move Wolf to the left island
-                new_state[1] = 0
 
-        elif action[0] == "[G,F]":
-            # Keep track of states
-            if new_state[4] == True:
-                # Goat moving on the right island
-                new_state[2] = 1
-                new_state[4] = not new_state[4]  # Changing to false
-            else:
-                # Goat moving on the left island
-                new_state[2] = 0
-                new_state[4] = not new_state[4]
-
-        elif action[0] == "[F]":
-            if new_state[4] == False:
-                # Farmer is on left island now and will move back to right island
-                new_state[4] = True
+        for x in action:
+            if x == "W":
+                new_state[1] = not new_state[1]
+            elif x == "G":
+                new_state[2] = not new_state[2]
+            elif x == "C":
+                new_state[3] = not new_state[3]
+            
+        #Change from t to f or f to t
+        new_state[4] = not new_state[4]
 
         return tuple(new_state)
 
     def actions(self, state):
-        # NEW FANCYPRINT !!!! This will return an array, with the action and the fancyprint.
-        #[ 'Action', 'Fancyprint']
+        possible_actions = ['Farmer','Wolf', 'Goat', 'Cabbage']  # 'FC', 'FW', 'FG'
+        Farmer = "Farmer"
+        Wolf = "Wolf"
+        Goat = "Goat"
+        Cabbage = "Cabbage"
 
-        # Make sure no bad movements occur
-        possible_actions = []  # 'FC', 'FW', 'FG'
-        possible_actions2 = []
         # (F, W, G, C, Position)
+        # FARMER 0
+        # WOLF 1
+        # GOAT 2
+        # CABBAGE 3
+        # POS 4
 
         # Possible actions per scenario on the right island:
         # False --> boat coming from right
@@ -77,66 +54,66 @@ class WolfGoatCabbage(Problem):
         # Nothing is on the right island..moving any possible item to the right
         if state[1] == 0 and state[2] == 0 and state[3] == 0 and state[4] == True:
             # Currently Farmer's action
-            possible_actions.append("[G,F]")
-            possible_actions.append("Farmer and Goat move to the right")
-            possible_actions2.append(possible_actions)
-            
+            # possible_actions.append("[G,F]")
+            # possible_actions.append('FC')
+            print(Farmer + " and " + Goat + " crosses")
+            possible_actions.remove("Wolf")
+            possible_actions.remove("Cabbage")
 
         if state[1] == 0 and state[2] == 1 and state[3] == 0 and state[4] == False:
             # Currently Farmer's action after moving goat..going back to left island
             # Coming from right island
-            possible_actions.append("[F]")
-            possible_actions.append("Farmer moves to the left")
-            possible_actions2.append(possible_actions)
+            print(Farmer + " crosses back")
+            possible_actions.remove("Wolf")
+            possible_actions.remove("Goat")
+            possible_actions.remove("Cabbage")
 
         # Goat is currently on the right island..boat has returned to the right island
         if state[1] == 0 and state[2] == 1 and state[3] == 0 and state[4] == True:
             # Currently Farmer's action to move wolf to left island
-            possible_actions.append("[W,F]")
-            possible_actions.append("Farmer and Wolf move to the right")
-            # Can't bring cabbage --> Goat will devour\
-            possible_actions2.append(possible_actions)
+            print(Farmer + " and " + Wolf + " crosses")
+            possible_actions.remove("Goat")
+            possible_actions.remove("Cabbage")
+            # Can't bring cabbage --> Goat will devour
 
         # Wolf and Goat is currently on the right island but cannot stay together
         if state[1] == 1 and state[2] == 1 and state[3] == 0 and state[4] == False:
             # Currently Farmer's action to return Goat on left island
-            possible_actions.append("[G,F]")
-            possible_actions.append("Farmer and Goat move to the left")
-            possible_actions2.append(possible_actions)
-            
+            #possible_actions.append("[G,F]")
+            print(Farmer + " and " + Goat +" crosses back")
+            possible_actions.remove("Wolf")
+            possible_actions.remove("Cabbage")
+
         # Wolf is currently on right island and is moving cabbage to right island
         if state[1] == 1 and state[2] == 0 and state[3] == 0 and state[4] == True:
             # Currently Farmer's action to move cabbage to right island
-            possible_actions.append("[C,F]")
-            possible_actions.append("Farmer and Cabbage move to the right")
-            possible_actions2.append(possible_actions)
-
-        # if state[1] == 1 and state[2] == 0 and state[3] == 0 and state[4] == False:
-        #     # Currently Farmer's action after moving cabbage...going back to left island
-        #     possible_actions.append("[F]")
-        #     possible_actions.append("Farmer moves to the left")
-        #     possible_actions2.append(possible_actions)
-
+            print(Farmer + " and " + Cabbage + " crosses")
+            possible_actions.remove("Wolf")
+            possible_actions.remove("Goat")
 
         if state[1] == 1 and state[2] == 0 and state[3] == 1 and state[4] == False:
             # Currently Farmer's action after moving cabbage...going back to left island
-            possible_actions.append("[F]")
-            possible_actions.append("Farmer moves to the left")
-            possible_actions2.append(possible_actions)
+            print(Farmer + " crosses back")
+            possible_actions.remove("Wolf")
+            possible_actions.remove("Goat")
+            possible_actions.remove("Cabbage")
 
         # Wolf and Cabbage currently on the right island
         if state[1] == 1 and state[2] == 0 and state[3] == 1 and state[4] == True:
             # Currently Farmer's action to move Goat to right island
-            possible_actions.append("[G,F]")
-            possible_actions.append("Farmer and Goat move to the right")
-            possible_actions2.append(possible_actions)
+            print(Farmer + " and " + Goat + " crosses")
+            possible_actions.remove("Wolf")
+            possible_actions.remove("Cabbage")
 
         if state[1] == 1 and state[2] == 1 and state[3] == 1 and state[4] == False:
             # Currently Farmer's action to move Goat to right island
-            possible_actions.append("[F,W,G,C]")
-            possible_actions.append("End of actions. Goal state reached.")
-            possible_actions2.append(possible_actions)
+            print("COMPLETED")
+            possible_actions.remove("Farmer")
+            possible_actions.remove("Goat")
+            possible_actions.remove("Wolf")
+            possible_actions.remove("Cabbage")
 
+        # print(state)   
         return possible_actions
 
     def h(self, node):
@@ -148,22 +125,65 @@ class WolfGoatCabbage(Problem):
 
 if __name__ == '__main__':
     initial = (1, 0, 0, 0, True)
-    wgc = WolfGoatCabbage()
-    sol = depth_first_graph_search(wgc)
-    # print(sol)
-    sol = breadth_first_graph_search(wgc)
-    # print(sol)
-
+    wgc = WolfGoatCabbage(initial)
+    print("This is DFS")
+    solution = depth_first_graph_search(wgc).solution()
+    print(solution) 
+    print("\n\nThis is BitchFS") 
+    solution = breadth_first_graph_search(wgc).solution()
+    print(solution)
     # Variable is the right side only and initial only
-    print(wgc.actions(initial)[1]) #Print second element, which is the action
-    pos_states = ["FG", "F", "FW", "FG", "FC", "F", "FG"]
-    current_state = initial
-    for x in pos_states:
-        # while wgc.goal_test:
-        temp_state = wgc.result(current_state, wgc.actions(current_state))
-        current_state = temp_state
-        # initial = (1, 0, 1, 0, False)
-        print(wgc.actions(current_state)[1]) #Print second element, which is the action
-        # print(current_state)
+    # print(wgc.actions(initial))
+
+    # current_state = initial
+    # while not wgc.goal_test(current_state):
+    #     temp_state = wgc.result(current_state, wgc.actions(current_state))
+    #     current_state = temp_state
+    #     # print(wgc.actions(current_state))
+    #     print(current_state)
 
 
+"""
+    Take the goat over
+    Return
+    Take the wolf or cabbage over
+    Return with the goat
+    Take the cabbage or wolf over
+    Return
+    Take goat over
+"""
+
+
+"""
+['G', 'F', 'W', 'G', 'C', 'F', 'G'] - DFS
+['F', 'W', 'F', 'G', 'C'] - BFS
+['F', 'G']
+['F']
+['F', 'W']
+['F', 'G']
+['F', 'C']
+['F']
+['F', 'G']
+[]
+"""
+
+"""
+FarmerGoat crosses
+Farmer crosses
+['Goat', 'Farmer', 'Wolf', 'Goat', 'Cabbage', 'Farmer', 'Goat']
+This is DFS
+
+FarmerGoat crosses
+Farmer crosses
+['Farmer', 'Wolf', 'Farmer', 'Goat', 'Cabbage']
+This is BFS
+FarmerGoat crosses
+['Farmer', 'Goat']
+"""
+
+"""
+# Nothing is on the right island..moving any possible item to the right
+        if state[1] == 0 and state[2] == 0 and state[3] == 0 and state[4] == True:
+            # Currently Farmer's action
+            possible_actions.append("Farmer and Goat cross")
+"""
